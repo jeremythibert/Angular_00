6d83735b9810434637d959a5b8d39da81feefc85
Cange <william.mp@sfr.fr>
2020-01-08 11:43:48 +0100

    01

---
+++ src/app/sign_up/sign_up.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+ selector: 'SignUp',
+ template: `
+ <h1>Mon premier Component</h1>
+ <p>en action</p>
+ `,
+ styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
+  
